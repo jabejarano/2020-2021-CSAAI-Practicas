3f89208f6d2385fb1386ceec4a69a6f80ce2a632
jabejarano <96siguenza@gmail.com>
2021-04-27 13:31:59 +0200

    primeras operaciones

--- P2/index.js
+++ P2/index.js
@@ -1,3 +1,37 @@
 //-- Punto de entrada: una vez cargada la página se llama a esta
-console.log("Aquí comienza tu código JS...")
-console.log("¡Que comiencen los juegos del JS!")
+console.log("Ejecutando JS...");
+
+//-- Elementos de la interfaz de la calculadora
+display = document.getElementById("display")
+boton1 = document.getElementById("boton1")
+boton2 = document.getElementById("boton2")
+suma = document.getElementById("suma")
+igual = document.getElementById("igual")
+clear = document.getElementById("clear")
+
+
+
+// -- Insertar digito 1
+boton1.onclick = () => {
+    display.innerHTML += boton1.value;
+  }
+
+//-- Insertar digito 2
+boton2.onclick = () => {
+    display.innerHTML += boton2.value;
+  }
+  
+//-- Insertar simbolo de sumar
+suma.onclick = () => {
+    display.innerHTML += suma.value;
+  }
+
+//-- Evaluar la expresion
+igual.onclick = () => {
+    display.innerHTML = eval(display.innerHTML);
+  }
+  
+//-- Poner a cero la expresion
+clear.onclick = () => {
+    display.innerHTML = "0";
+  }
