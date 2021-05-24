1aa23aa363fd7a864b691408dc1ce26bac09e616
jabejarano <96siguenza@gmail.com>
2021-05-24 18:06:05 +0200

    probando escenario y pelota

--- P3/pong.js
+++ P3/pong.js
@@ -3,25 +3,67 @@ console.log("Ejecutando JS...");
 const canvas = document.getElementById("canvas");
 
 //-- Definir el tamaño del convas
-canvas.width = 200;
-canvas.height = 100;
+canvas.width = 500;
+canvas.height = 250;
 
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
 
-ctx.beginPath();
+//-- Velocidades del objeto
+let velx = 3;
+let vely = 1;
+
+
+
+
+//-- Funcion principal de animacion
+function update() 
+{
+    console.log("test");
+    //-- Algoritmo de animacion:
+    //-- 1) Actualizar posicion del  elemento
+    //-- (física del movimiento rectilineo uniforme)
+  
+     //-- Condicion de rebote en extremos verticales del canvas
+     if (x < 0 || x >= (canvas.width - 20) ) {
+      velx = -velx;
+    }
+  
+    //-- Condición de rebote en extremos horizontales del canvas
+    if (y <= 0 || y > 100) {
+      vely = -vely;
+    }
+  
+    //-- Actualizar la posición
+    x = x + velx;
+    y = y + vely;
+  
+    //-- 2) Borrar el canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    //-- 3) Dibujar los elementos visibles
+    ctx.beginPath();
     //-- Dibujar un circulo: coordenadas x,y del centro
     //-- Radio, Angulo inicial y angulo final
-    ctx.arc(100, 50, 30, 2, 2 * Math.PI);
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 0;
     ctx.fillStyle = 'yellow';
-
-    //-- Dibujar el trazo
-    ctx.stroke()
 
     //-- Dibujar el relleno
     ctx.fill()
-    
-ctx.closePath()
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
