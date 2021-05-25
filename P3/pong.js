4e74ae9d1a9609c97f41804417a89fe1a205dc35
jabejarano <96siguenza@gmail.com>
2021-05-25 12:13:34 +0200

    probando saque

--- P3/pong.js
+++ P3/pong.js
@@ -9,16 +9,23 @@ canvas.height = 250;
 //-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
+const ESTADO = {
+  START: 0,
+  INIT: 1,
+  INGAME: 2,
+  FINISH: 3,
+}
+
+let estado = ESTADO.START;
+
 //-- Coordenadas del objeto
-let x = 0;
-let y = 10;
+let x = 250;
+let y = 200;
 
 //-- Velocidades del objeto
-let velx = 3;
-let vely = 1;
-
-
-
+let velx = -3;
+let vely = -1;
+ 
 
 //-- Funcion principal de animacion
 function update() 
@@ -34,24 +41,41 @@ function update()
     }
   
     //-- Condición de rebote en extremos horizontales del canvas
-    if (y <= 0 || y > 100) {
+    if (y <= 0) {
       vely = -vely;
+    }
+
+    //-- Codición si la bola pasa la raqueta de abajo se reinicia el movimiento
+    if (y >= 250 ) {
+      estado = ESTADO.START;
+      console.log("fuera")
+    }
+
+
+    window.onkeydown = (e) => {
+    if (e.key == 'Enter' && estado == ESTADO.START){
+      estado = ESTADO.INIT;
+      console.log("SAQUE")
+      }
     }
   
     //-- Actualizar la posición
-    x = x + velx;
-    y = y + vely;
+    if (estado == ESTADO.INIT) {
+      x = x + velx;
+      y = y + vely;
+    }
   
     //-- 2) Borrar el canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
     //-- 3) Dibujar los elementos visibles
     ctx.beginPath();
     //-- Dibujar un circulo: coordenadas x,y del centro
     //-- Radio, Angulo inicial y angulo final
+    if (estado == ESTADO.INIT) {
     ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    }
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 0;
+    ctx.lineWidth = 3;
     ctx.fillStyle = 'yellow';
 
     //-- Dibujar el relleno
