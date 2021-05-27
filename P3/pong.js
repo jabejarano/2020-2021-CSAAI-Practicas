3d80f109f40e36163abb7a68376bddcb338d1e48
jabejarano <96siguenza@gmail.com>
2021-05-27 11:53:57 +0200

    ladrillos dibujados

--- P3/pong.js
+++ P3/pong.js
@@ -40,15 +40,40 @@ let leftPressed = false;
 
 //-- Constantes de los ladrillos
 const LADRILLO = {
-  F: 2,  // Filas
-  C: 3,  // Columnas
-  w: 30,
-  h: 20,
-  origen_x: 0,
-  origen_y: 0,
+  F: 5,  // Filas
+  C: 9,  // Columnas
+  w: 60,
+  h: 15,
+  origen_x: 8,
+  origen_y: 60,
   padding: 5,
   visible: true
 };
+
+  //-- Estructura de los ladrillos
+//-- Creación de los ladrillos, que inicialmente está vacío
+//-- en el objeto ladrillos, que inicialmente está vacío
+const ladrillos = [];
+
+//-- Recorrer todas las filas. La veriable i toma valores de 0 hasta F-1 (número de filas)
+for (let i = 0; i < LADRILLO.F; i++) {
+    ladrillos[i] = [];  //-- Inicilizar la fila. Las filas son a su vez Arrays que inicialmente están vacíos
+
+    //-- Recorrer las C columnas de la fila i. La variable j toma valores de 0 hasta C-1 (numero de columnas)
+    for (let j = 0; j < LADRILLO.C; j++) {
+
+        //-- Calcular valores para el ladrillo de la fila i y la columna j
+        //-- Algunos valores son constantes. Otros dependen de i y j
+      ladrillos[i][j] = {
+          x: (LADRILLO.w + LADRILLO.padding) * j +  LADRILLO.origen_x,
+          y: (LADRILLO.h + LADRILLO.padding) * i + LADRILLO.origen_y,
+          w: LADRILLO.w,
+          h: LADRILLO.h,
+          padding: LADRILLO.padding,
+          visible: LADRILLO.visible
+        };
+    }
+}
 
 //-- Funcion principal de animacion
 function update() 
@@ -64,7 +89,7 @@ function update()
     }
   
     //-- Condición de rebote en extremos horizontales del canvas
-    if (y <= 0) {
+    if (y <= 60) {
       vely = -vely;
     }
 
@@ -167,37 +192,8 @@ function update()
   // ctx.fillText("Puntuación: " + scores, 10, 40);
   ctx.fillText("Vidas: " + lifes, 430, 40);
 
- 
-  //-- Estructura de los ladrillos
-//-- Creación de los ladrillos, que inicialmente está vacío
-//-- en el objeto ladrillos, que inicialmente está vacío
-const ladrillos = [];
-
-//-- Recorrer todas las filas. La veriable i toma valores de 0 hasta F-1 (número de filas)
-for (let i = 0; i < LADRILLO.F; i++) {
-    ladrillos[i] = [];  //-- Inicilizar la fila. Las filas son a su vez Arrays que inicialmente están vacíos
-
-    //-- Recorrer las C columnas de la fila i. La variable j toma valores de 0 hasta C-1 (numero de columnas)
-    for (let j = 0; j < LADRILLO.C; j++) {
-
-        //-- Calcular valores para el ladrillo de la fila i y la columna j
-        //-- Algunos valores son constantes. Otros dependen de i y j
-      ladrillos[i][j] = {
-          x: (LADRILLO.w + LADRILLO.padding) * j,
-          y: (LADRILLO.h + LADRILLO.padding) * i,
-          w: LADRILLO.w,
-          h: LADRILLO.h,
-          padding: LADRILLO.padding,
-          visible: LADRILLO.visible
-        };
-    }
-}
-
-ladrillos[0][1].visible = false;
-
-
-//-- Dibujar ladrillos
-for (let i = 0; i < LADRILLO.F; i++) {
+  //-- Dibujar ladrillos
+  for (let i = 0; i < LADRILLO.F; i++) {
     for (let j = 0; j < LADRILLO.C; j++) {
 
       //-- Si el ladrillo es visible se pinta
@@ -209,7 +205,7 @@ for (let i = 0; i < LADRILLO.F; i++) {
         ctx.closePath();
       }
     }
-}
+  }
 
   //-- 4) Volver a ejecutar update cuando toque
   requestAnimationFrame(update);
