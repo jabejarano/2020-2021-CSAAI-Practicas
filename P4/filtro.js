e75089b1b4fd1ab1b769435305ed8a8b79b975b6
jabejarano <96siguenza@gmail.com>
2021-05-29 18:51:59 +0200

    Rojo y verde funciona

--- P4/filtro.js
+++ P4/filtro.js
@@ -41,12 +41,13 @@ img.onload = function () {
   //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
   console.log("Imagen lista...");
+};
 
-
-    //-- Funcion de retrollamada del deslizador
-    deslizadorR.oninput = () => {
+function umbrales(){
+    
     //-- Mostrar el nuevo valor del deslizador
     range_valueR.innerHTML = deslizadorR.value;
+    range_valueG.innerHTML = deslizadorG.value;
   
     //-- Situar la imagen original en el canvas
     //-- No se han hecho manipulaciones todavia
@@ -59,45 +60,31 @@ img.onload = function () {
     let data = imgData.data
   
     //-- Obtener el umbral de rojo del desliador
-    umbral = deslizadorR.value
+    umbralR = deslizadorR.value
+    umbralG = deslizadorG.value
+
   
     //-- Filtrar la imagen según el nuevo umbral
     for (let i = 0; i < data.length; i+=4) {
-      if (data[i] > umbral)
-        data[i] = umbral;
+      if (data[i] > umbralR){
+        data[i] = umbralR;
     }
-  
+    if (data[i+1] > umbralG){
+        data[i+1] = umbralG;
+    }
+    }  
     //-- Poner la imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
+  } 
+  
+    //-- Funcion de retrollamada de los deslizadores
+    deslizadorR.oninput = () => {
+    umbrales();
+  }
+    deslizadorG.oninput = () => {
+    umbrales();
   }
 
-     //-- Funcion de retrollamada del deslizador
-     deslizadorG.oninput = () => {
-        //-- Mostrar el nuevo valor del deslizador
-        range_valueG.innerHTML = deslizadorG.value;
-      
-        //-- Situar la imagen original en el canvas
-        //-- No se han hecho manipulaciones todavia
-        ctx.drawImage(img, 0,0);
-      
-        //-- Obtener la imagen del canvas en pixeles
-        let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      
-        //-- Obtener el array con todos los píxeles
-        let data = imgData.data
-      
-        //-- Obtener el umbral de rojo del desliador
-        umbral = deslizadorG.value
-      
-        //-- Filtrar la imagen según el nuevo umbral
-        for (let i = 0; i < data.length; i+=4) {
-          if (data[i+1] > umbral)
-            data[i+1] = umbral;
-        }
-      
-        //-- Poner la imagen modificada en el canvas
-        ctx.putImageData(imgData, 0, 0);
-      }
-    }
+
   
   console.log("Fin...");
