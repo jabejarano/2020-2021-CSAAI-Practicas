943132b69536dad96bd5ebd1892bb40a2b4d255d
jabejarano <96siguenza@gmail.com>
2021-05-31 14:16:15 +0200

    Implementación video 3

--- P5/index.js
+++ P5/index.js
@@ -4,6 +4,7 @@ const video1 = document.getElementById("video1");
 const video2 = document.getElementById("video2");
 const btn_video1 = document.getElementById("btn_video1");
 const btn_video2 = document.getElementById("btn_video2");
+const btn_video3 = document.getElementById("btn_video3");
 const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
 const btn_src_off = document.getElementById("btn_src_off");
@@ -16,6 +17,8 @@ video1.width=200;
 video1.height=100;
 video2.width=200;  
 video2.height=100;
+video3.width=200; 
+video3.height=100;
 
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "test.png";
@@ -25,6 +28,7 @@ const TEST_IMAGE_URL = "test.png";
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
 video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
@@ -32,7 +36,7 @@ btn_src_on.onclick = () => {
   //-- Establecer la fuente de la cámara 1
   video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
   video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
- 
+  video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4";
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
@@ -40,10 +44,11 @@ btn_src_on.onclick = () => {
   video2.currentTime = 0;
   video2.play();
   video2.muted = true;
+  video3.currentTime = 0;
+  video3.play();
+  video3.muted = true;
 
-  //-- Y en silencio...
-  video1.muted;
-  video2.muted;
+ 
 
 
   //-- En la emisión en directo ponemos la imagen de prueba
@@ -61,14 +66,19 @@ btn_video1.onclick = () => {
     directo.src = video1.src;
     directo.currentTime = video1.currentTime;
     directo.play();
-    directo.poster=null;
+    
 };
 
 btn_video2.onclick = () => {
     directo.src = video2.src;
     directo.currentTime = video2.currentTime;
     directo.play();
-    directo.poster=null;
+};
+
+btn_video3.onclick = () => {
+    directo.src = video3.src;
+    directo.currentTime = video3.currentTime;
+    directo.play();
 };
 
 btn_src_off.onclick = () => {
@@ -76,4 +86,5 @@ btn_src_off.onclick = () => {
     directo.poster = TEST_IMAGE_URL;
     video1.src = TEST_IMAGE_URL;
     video2.src = TEST_IMAGE_URL;
+    video3.src = TEST_IMAGE_URL;
 }
